d2c19e3b94a4378b90a59873008263a8e23042ea
Davis Hilton <davised2021@gmail.com>
2022-04-10 12:05:08 -0400

    step 4/5 completed but needs fixing

--- js/index.js
+++ js/index.js
@@ -18,24 +18,42 @@
 
 // =======Step 4: Adding Tasks With The Form =======
 
-const newTask = new TaskManager(); //Initialization of New Task
+const newTask = new TaskManager(1); //Initialization of New Task
 
+/*===============Test to see if works ======================
+newTask.addTask("mop", "mopping", "davis", 07 / 08 / 1989);
+console.log(newTask.tasks)
+=========================================================*/
 
-
+//Step 4.1
 const newTaskForm = document.querySelector('#newTaskForm');
 
 // Add an 'onsubmit' event listener
+//Step 4.2/4.3
 newTaskForm.addEventListener("submit", (e) => {
-    // Prevent default action
-         e.preventDefault();
- });
+	// Prevent default action
+	e.preventDefault();
 
+	// Select the inputs
+	const newTaskNameInput = document.querySelector("#newTaskNameInput");
+	const newTaskDescription = document.querySelector("#newTaskDescription");
+	const newTaskAssignedTo = document.querySelector("#newTaskAssignedTo");
+	const newTaskDueDate = document.querySelector("#newTaskDueDate");
+	const errorMessage = document.querySelector("#alertMessage");
+	//needed to add somewhere!!!!!!!!!!
 
- // Select the inputs
-    const newTaskNameInput = document.querySelector('#newTaskNameInput');
-    const newTaskDescription = document.querySelector('#newTaskDescription');
-    const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
-    const newTaskDueDate = document.querySelector('#newTaskDueDate');
-    const errorMessage = document.querySelector('#alertMessage');
-   //needed to add somewhere!!!!!!!!!!
+	/*Step 4.3 We need to get the values of input from form
+  we alraedy trageted them with the variables above
+so now we have to put thier 'values' into another
+varable with a .value at the end.*/
+  const name = newTaskNameInput.value;
+	const description = newTaskDescription.value;
+	const assignedTo = newTaskAssignedTo.value;
+	const dueDate = newTaskDueDate.value;
+});
 
+newTask.addTask(this.name, this.description,this.assignedTo,this.dueDate);
+console.log(newTask.addTask());
+
+//step 5.3.2
+newTask.render();
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,32 +1,91 @@
-class TaskManager {   //step 2.1.a
-  // Set up the tasks and currentId property in the contructor
-  constructor(currentId = 0) {
-    this.currentId = currentId;
-    this.tasks = [];  //step 2.2.a
-  }
+//=========================================================
+//When create a function why not work if use {}?
 
-  // step 2.1.b / step 2.2.b (in index.js)
+const createTaskHtml = (name, description, assignedTo, dueDate, status) => 
+                   `<div class="card-body">
+                    <!--================ Task 1 =================--> 
+                    <p class="card-title">Task 1</p>
+                    <p class="card-text">Name: ${name}</p>
+                    <p class="card-text">Description: ${description}</p>
+                    <p class="card-text">Assigned To: ${assignedTo}</p>
+                    <p class="card-text">Due Date: ${dueDate}</p>
+                    <p class="card-text">Status:${status} </p>
+                    <div class="btn-group"><a href="#" class="btn btn-primary">Edit</a></div>
+                    <div class="btn-group"><a href="#" class="btn btn-primary">Delete</a></div>
+                   </div>
+                   `
+;
 
-  //step 3 - 
-  // Create the addTask method to 
-  //add task to this.task array. This is done by 
-  //creating a task{}(task object) and using our
-  // input questions as perameters. 
-  //The questions will be the keys and answers are values
-  // step 3.3
- 
-  addTask(name, description, assignedTo, dueDate) {
-    const task = {
-      id: this.currentId++, // step 4 
-      name: name,
-      description: description,
-      assignedTo: assignedTo,
-      dueDate: dueDate,
-      status: 'TODO'   //default status
-    };
+console.log(createTaskHtml("mop", "mop over", "davis", "07/06/2003", "todo"));
 
-    this.tasks.push(task); //step 3.5 (this is what it is pushed to array above)
-  }
-}
+;
+
+
+//=========================================================
+class TaskManager {
+	//step 2.1.a
+	// Set up the tasks and currentId property in the contructor
+	constructor(currentId = 0) {
+		this.currentId = currentId;
+		this.tasks = []; //step 2.2.a
+	}
+
+	// step 2.1.b / step 2.2.b (in index.js)
+
+	//step 3 -
+	// *** Create the addTask method to ****
+	//add task to this.task array. This is done by
+	//creating a task{}(task object) and using our
+	// input questions as perameters.
+	//The questions will be the keys and answers are values
+	// step 3.3
+
+	addTask(name, description, assignedTo, dueDate) {
+		const task = {
+			id: this.currentId++, // step 4
+			name: name,
+			description: description,
+			assignedTo: assignedTo,
+			dueDate: dueDate,
+			status: "TODO", //default status
+		};
+
+		this.tasks.push(task); //step 3.5 (this is what it is pushed to array above)
+	}
+
+	/*===================================
+ create a **render() method**. Step5.2
+ ======================================*/
+  render() {
+		const tasksHtnmList = []; //Step 5.2.2
+		let renderTask = this.tasks.forEach((i) => {
+			const task = this.tasks[i]; //step 5.2.3.i
+
+			const date = new Date(task.dueDate); //Step 5.2.3.ii
+			const formattedDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`; //step 5.2.3.iii
+
+			// Pass the task id as a parameter
+			const taskHtml = createTaskHtml(
+				//step 5.2.3.iv
+				task.id,
+				task.name,
+				task.description,
+				task.assignedTo,
+				formattedDate,
+				task.status
+			);
+			tasksHtmlList.push(taskHtml); //step 5.2.3.v
+		}); //for each() ending
+
+		const tasksHtml = tasksHtmlList.join("\n"); ////step 5.2.4
+
+		const tasksList = document.querySelector("#tasksList");
+		tasksList.innerHTML = tasksHtml; //Step 5.2.6
+	}//render() ending
+  } //class ending }
+
+
+
+
 
 
