55c13f0b91efc05c2478e31be297b5ca0ce385f8
Tanya Mann <thombre99@protomail.com>
2022-04-14 09:27:24 -0600

    updated code to include  tasks 6,7,8,9

--- js/index.js
+++ js/index.js
@@ -1,50 +1,113 @@
-// let tasks = new TaskManager();
-// console.log(tasks)
 
+const taskManager = new TaskManager(0);
 
+taskManager.load();
 
-//add an 'onsubmit' event listener
-
-const newTaskForm = document.querySelector('#newTaskForm');
-
-newTaskForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-
-//select the inputs
-const newTaskForm = document.querySelector('#newTaskForm')
-const newTaskNameInput = document.querySelector('#newTaskNameInput');
-const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
-const newTaskDueDate = document.querySelector('#newTaskDueDate');
-const errorMessage = document.querySelector('#alertMessage');
-
-
-// get the values of the inputs
-const name = newTaskNameInput.value;
-const description = newTaskDescription.value;
-const assignedTo = newTaskAssignedTo.value;
-const dueDate = newTaskDueDate.value;
-
-//add the task to the task manager
-taskManager.addTask(name, description, assignedTo, dueDate);
-
-//Clear the form
-newTaskNameInput.value = '';
-newTaskDesciption.value = '';
-newTaskDueDue.value = '';
-
-taskManager.addTask(name, description, assignedTo, dueDate);
- 
-//render the tasks
 taskManager.render();
 
-newTaskNameInput.value = '';
-newTaskDecription.value = '';
-newTaskAssignedTo.value = '';
-newTaskDueDate.value = '';
+const newTaskForm = document.querySelector("#newTaskForm");
+
+// newTaskForm.addEventListener('submit', (event) => {
+//     event.preventDefault();
+
+// })
+
+const newTaskNameInput = document.getElementById('newTaskNameInput');
+const newTaskDescription = document.getElementById('newTaskDescription');
+const newTaskAssignedTo = document.getElementById('newTaskAssignedTo');
+const newTaskDueDate = document.getElementById('newTaskDueDate');
+const alert = document.querySelector('.alert');
+
+
+// validation code
+const validFormFieldInput = () => {
+    if (!newTaskNameInput.value || !newTaskDescription.value || !newTaskAssignedTo.value || !newTaskDueDate.value) {
+        let errorList = [];
+        if (!newTaskNameInput.value) {
+            errorList.push(" Name");
+        }
+        if (!newTaskDescription.value) {
+            errorList.push(" Description");
+        }
+        if (!newTaskAssignedTo.value) {
+            errorList.push(" Assigned To");
+        }
+        if (!newTaskDueDate.value) {
+            errorList.push(" Due Date");
+        }
+        alert.classList.add("invalid-form-alert");
+        alert.classList.remove("hide-alert");
+        alert.innerHTML = `All fields are required! Please check the following field(s):${errorList.toString()}`;
+        return false;
+    }
+    else {
+        if (alert.classList.contains("invalid-form-alert")) {
+            alert.classList.remove("invalid-form-alert");
+            console.log("yay");
+        }
+        if (!alert.classList.contains("hide-alert")) {
+            alert.classList.add("hide-alert");
+        }
+        console.log(`name: ${newTaskNameInput.value} description: ${newTaskDescription.value} assigned: ${newTaskAssignedTo.value} due date: ${newTaskDueDate.value}`);
+        return true;
+    }
+}
+
+newTaskForm.addEventListener("submit", (e) => {
+e.preventDefault();
+
+
+// Validation code when click submit button
+
+let isFormValidation = validFormFieldInput();
+
+if(isFormValidation){
+    
+    taskManager.addTask(newTaskNameInput.value,newTaskDescription.value,newTaskAssignedTo.value,newTaskDueDate.value);
+    // console.log(taskManager.tasks);
+    
+    taskManager.render();
+    newTaskNameInput.value = '';
+    newTaskDescription.value = '';
+    newTaskAssignedTo.value = '';
+    newTaskDueDate.value = '';
+}
 
 });
 
-const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
-const assignedTo = newTaskAssignedInput.value;
-console.log("assignedTo:  "+assignedTo);
+
+const tasksListId = document.querySelector("#tasksList");
+
+tasksListId.addEventListener("click", (event) => {
+if (event.target.classList.contains("done-button")) {
+    const parentTask = event.target.parentElement.parentElement;
+    // console.log(parentTask);
+    const taskId = Number(parentTask.dataset.taskId);
+    const taskCardInfo = taskManager.getTaskById(taskId);
+    taskCardInfo.status = "DONE";
+
+    taskCardInfo.save();
+
+    taskManager.render();
+}
+//check if a "delete" button was clicked
+if (event.target.classList.contains('delete-button')) {
+//get the parent Task 
+const parentTask = event.target.parentElement.parentElement
+
+//get the taskId of the parent Task
+const taskId = Number(parentTask.dataset.taskId);
+
+//Delete the task
+taskManager.deleteTask(taskId);
+
+//save the tasks to localStorage
+taskManager.save();
+
+//render the tasks
+taskManager.render();
+
+}
+
+});
+
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,74 +1,134 @@
-//create the HTmL for a task
-const createTaskHtml = (name, description, assignedTo, dueDate, status) =>  const htmlTask1 = `
-<div class="col-12 col-md-6 col-lg-4">
-   <div class="card g-4">
-      <div class="card-body">
-        <!--================ Task 1 =================--> 
-        <p class="card-title">Task 1</p>
-        <p class="card-text">${name}: Kharmalina Wireframe</p>
-        <p class="card-text">${description}: Create a wireframe in Figma and share with team</p>
-        <p class="card-text">${assignedTo}: Kharmalina </p>
-        <p class="card-text">${dueDate}: 02/14/2022</p>
-        <p class="card-text">${status}: DONE</p>
-        <div class="btn-group"><a href="#" class="btn btn-primary">Edit</a></div>
-        <div class="btn-group"><a href="#" class="btn btn-primary">Delete</a></div>
-       </div>
-    </div>
- </div>
- 
- return htmlTask1;
+const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
+    const htmlTask1 = `
+    <li data-task-id=${id} class="d-flex row g-4 justify-content-center align-items-center">
+        <div class="col-12 col-md-8 col-lg-9">
+        <div class="card g-4" data-task-id=${id}>
+            <div class="card-body"> 
+                <div class="d-flex w-100 align-items-center justify-content-between align-items-center">
+                <h4 class="card-text"><b>Name:</b> ${name}</h4>
+                <h4><span class="badge ${status === 'TODO' ? 'bg-danger' : 'bg-success'}">${status}</span></h4>
+                </div>
+                <h4 class="card-text"><b>Description:</b> ${description}</h4>
+                <h4 class="card-text"><b>Assigned To:</b> ${assignedTo}</h4>
+                <h4 class="card-text"><b>Due Date:</b> ${dueDate}</h4>
+                <button type="button" class="btn btn-success done-button ${status === 'TODO' ? 'visible' : 'invisible'}">Mark as Done</button>
+                <button class="btn btn-outline-danger delete-button">Delete</button>
+            </div>
+        </div>
+        </div>
+    </li>
 `;
+return htmlTask1;
+};
+// const taskHtml = createTaskHtml("Kharmalina", "Task 5 completion", "Kharmalina", "04/08/2022", "IN PROGRESS")
+// console.log(taskHtml);
 
-
+// taskManager class that handles adding and rendering a new task every time submit button is clicked
 class TaskManager {
-    constructor(currentId = 0){
+    constructor(currentId = 0) {
         this.tasks = [];
-        this.currentId = currentId;  
-    } 
-   
-       
+        this.currentId = currentId;
+    }
 
-        addTask (name, description, assignedTo, dueDate){
-            const task = {
-            id: this.currentId++,
-            name: name,
-            description: description,
-            assignedTo: assignedTo,
-            dueDate: dueDate,
-            status: 'TODO'
-        };
-        this.tasks.push(task);
-           
+addTask(name, description, assignedTo, dueDate) {
+
+    const taskCardInfo = {
+        id: this.currentId++,
+        name: name,
+        description: description,
+        assignedTo: assignedTo,
+        dueDate: dueDate,
+        status: "TODO"
         }
-  
-//create the render method
-render() {
-    //create an array to store the tasks HTML
-    const tasksHtmlList = [];
+        this.tasks.push(taskCardInfo);
+    }
+//create the deleteTask method 
+deleteTask(taskId) {
+    //create an empty array and store it in a new variable, newTasks
+    const newTasks = [];
 
-    //loop over tasks and create the html, storing it in the array
-    for(let i=0; i<this.tasks.length; i++) {
+    //loop over the tasks
+    for (let i=0; i< this.tasks.length; i++) {
         //get the current task in the loop
         const task = this.tasks[i];
 
-        //format Date
-        const date = new Date(task.dueDate);
-        const formattedDate = date.getDate() + '/' + (date.getMonth() +1) + '/' + date.getFullYear();
-        
-        //create the task html
-        const taskHtml = createTaskHtml(task.name, task,desciption, task.assignedTo, formattedDate, task.status);
+        //check if the task id is not the task id passed in as a parameter
+        if (task.id !== taskId) {
+            //push the task to the newTasks array
+            newTasks.push(task);
+        }
+    }
+    //set this.tasks to newTasks
+    this.tasks = newTasks;
+}
 
-        //push it to the tasksHtmlList array
+getTaskById(taskId) {
+    let foundTask;
+
+    for(let i=0; i<this.tasks.length; i++) {
+        const taskCardInfo = this.tasks[i];
+
+        if(taskCardInfo.id === taskId) {
+            foundTask = taskCardInfo;
+        }
+    }
+
+    return foundTask;
+}
+
+
+render() {
+    const tasksHtmlList = [];
+    
+    for(let i=0; i<this.tasks.length; i++) {
+        // stores the current task 
+        const taskCardInfo = this.tasks[i];
+        // Date object
+        const date = new Date(taskCardInfo.dueDate);
+        // human-readable code of the dueDate property
+        const formattedDate = `${date.getMonth() + 1} / ${date.getDate()} / ${date.getFullYear()}`;
+
+        const taskHtml = createTaskHtml(taskCardInfo.id, taskCardInfo.name, taskCardInfo.description, taskCardInfo.assignedTo, formattedDate, taskCardInfo.status);
         tasksHtmlList.push(taskHtml);
+    }
 
-}
-    //create the tasksHtml by joining each item in the tasksHtmlList array
-    tasksHtmlList.push(taskHtml);
-
-    //set the inner html of the tasksList on the page
-    const tasksList = document.querySelector('#tasksList');
-    tasksList.innerHtml = tasksHtml;
-
-}
+    const tasksHtml = tasksHtmlList.join("<br>");
+    const tasksList = document.getElementById("tasksList");
+    tasksList.innerHTML = tasksHtml;
 }
 
+//create the save method
+save() {
+//create a JSON string of the tasks
+const tasksJson = JSON.stringify(this.tasks);
+
+//Store the JSON string in localStorage
+localStorage.setItem('tasks', tasksJson);
+
+//Convert the currentId to a string;
+const currentId = String(this.currentId);
+
+//store the currentId in localStorage
+localStorage.setItem('currentId', currentId);
+}
+
+//create the load method
+load() {
+//Check if any tasks are saved in localStorage
+if (localStorage.getItem('tasks')) {
+   //get the JSON string of tasks in localStorage
+   const tasksJson = localStorage.getItem('tasks');
+   //convert it to an array and store it in our TaskManager
+   this.tasks = JSON.parse(tasksJson); 
+}
+//check if the currentId is saved in localStorage
+if (localStorage.getItem('currentId')) {
+    //get the currentId string in localStorage
+    const currentId = localStorage.getItem('currentId');
+
+    //convert the currentId to a number and store it in our TaskManager
+    this.currentId = Number(currentId);
+        }
+
+    }
+};
